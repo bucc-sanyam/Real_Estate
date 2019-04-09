9b817dc9fbda6087fd2b4d72eda7df1a5d681ad7
Sanyam Gupta <sanyamgupta2307@gmail.com>
2019-04-09 23:15:17 +0530

    Working Property features

--- Real Estate/Real_Estate/Property/urls.py
+++ Real Estate/Real_Estate/Property/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('register/', PropertyOperations.as_view(), name='register_property'),
-    path('update/<int:pk>', UpdateProperty.as_view(), name='update_property'),
+    path('register/', RegisterProperty.as_view(), name='register_property'),
+    path('update/', UpdateProperty.as_view(), name='update_property'),
+    path('update/<int:pk>', UpdatePropertyForm.as_view(), name='update_property_form'),
     path('list/', ViewAllProperty.as_view(), name='list_all_property'),
     path('details/<int:pk>', ViewSpecificProperty.as_view(), name='detailed_view_property'),
+    path('delete/<int:pk>', DeleteProperty.as_view(), name='delete_property'),
+    path('delete/', DeletePropertyList.as_view(), name='delete_property_list'),
 ]
--- Real Estate/Real_Estate/Property/views.py
+++ Real Estate/Real_Estate/Property/views.py
@@ -1,7 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 from django.views import View
 from django.views.generic import FormView, DetailView, UpdateView, ListView, DeleteView
 from django.contrib.auth.models import User
@@ -9,40 +11,52 @@ from .forms import PropertyForm
 from .models import Properties
 
 
-class PropertyOperations(LoginRequiredMixin, FormView):
+class RegisterProperty(LoginRequiredMixin, FormView):
     form_class = PropertyForm
-    template_name = 'properties.html'
+    template_name = 'register_property.html'
 
     def get(self, request):
         if request.user.profile.is_seller:
-            return render(self.request, 'properties.html', context={'form': PropertyForm})
+            return render(self.request, 'register_property.html', context={'form': PropertyForm})
         else:
             message = 'You are not a seller, please login with a seller account.'
             header = 'Sorry'
-            return render(self.request, template_name='properties.html', context={'message': message, 'header': header})
+            return render(self.request, template_name='register_property.html',
+                          context={'message': message, 'header': header})
 
     def form_valid(self, form):
         form.instance.property_seller_name = self.request.user
         form.save()
-        message = 'You have successfully posted a property !'
-        return render(self.request, template_name='properties.html', context={'message': message})
+        messages.add_message(self.request, messages.INFO, 'You have successfully posted a property !')
+        return redirect('list_all_property')
 
     def form_invalid(self, form):
-        return render(self.request, template_name='properties.html', context={'form': form})
+        return render(self.request, template_name='register_property.html', context={'form': form})
 
 
-class UpdateProperty(LoginRequiredMixin, UpdateView):
+class UpdateProperty(LoginRequiredMixin, ListView):
+    model = Properties
+    template_name = 'update_property.html'
+
+
+class UpdatePropertyForm(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Properties
     fields = ['property_title', 'property_address', 'property_city', 'property_state', 'property_pin',
               'property_price', 'property_bedroom', 'property_bathroom', 'property_sq_feet', 'property_lot_size',
               'property_garage', 'property_description', 'property_image1', 'property_image2', 'property_image3',
               'property_image4', 'property_image5']
-    template_name = 'update_property.html'
+    template_name = 'update_property_form.html'
+    success_url = reverse_lazy('list_all_property')
+
+    def test_func(self):
+        property = self.get_object()
+        if self.request.user == property.property_seller_name:
+            return True
+        return False
 
 
 class ViewAllProperty(ListView):
     model = Properties
-    paginate_by = 2
     template_name = 'property_list.html'
 
 
@@ -51,15 +65,21 @@ class ViewSpecificProperty(DetailView):
     template_name = 'property_details.html'
 
 
-# class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-#     model = Properties
-#     success_url = '/'
-#
-#     def test_func(self):
-#         property = self.get_object()
-#         if self.request.user == property.property_seller_name:
-#             return True
-#         return False
+class DeletePropertyList(LoginRequiredMixin, ListView):
+    model = Properties
+    template_name = 'delete_property_list.html'
+
+
+class DeleteProperty(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Properties
+    template_name = 'properties_confirm_delete.html'
+    success_url = reverse_lazy('list_all_property')
+
+    def test_func(self):
+        property = self.get_object()
+        if self.request.user == property.property_seller_name:
+            return True
+        return False
 
 
 def search_property(request):
