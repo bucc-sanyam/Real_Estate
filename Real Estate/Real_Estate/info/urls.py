cc12406df286ff5b8f2b3191b554376403c28b95
Sanyam Gupta <sanyamgupta2307@gmail.com>
2019-04-16 02:30:35 +0530

    Working Completely, added Contact Us page.

--- Real Estate/Real_Estate/info/urls.py
+++ Real Estate/Real_Estate/info/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from info.views import contact
+from info.views import contact, handleEnquiry
 
 urlpatterns = [
     path('contact/', contact, name='contact'),
+    path('contact/enquiry/', handleEnquiry, name="handleEnquiry")
 ]
--- Real Estate/Real_Estate/info/views.py
+++ Real Estate/Real_Estate/info/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
+
+
 def contact(request):
     return render(request, 'contact.html')
+
+
+def handleEnquiry(request):
+    fullname = request.POST['fullname']
+    subject = request.POST['subject']
+    message = request.POST['message']
+    email = request.POST['email']
+    send_mail(
+        'Subject : ' + subject,
+        'Message : ' + message + '\nEnquiry by : ' + fullname,
+        email,
+        ['sanyam.gupta@tothenew.com'],
+        fail_silently=False,
+    )
+    messages.success(request, f"Hey {fullname}! Your enquiry has been received. We'll contact you ASAP!")
+    return redirect("contact")
